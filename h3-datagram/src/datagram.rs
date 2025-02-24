9fb57c4c514ad039165a2457de1466bd1707abd0
Samuel Hitz <hitz@anapaya.net>
2025-02-24 07:45:32 +0100

    h3-datagrams: add client library

--- h3-datagram/src/datagram.rs
+++ h3-datagram/src/datagram.rs
@@ -1,5 +1,20 @@
+use std::{
+    future::Future,
+    marker::PhantomData,
+    task::{ready, Context, Poll},
+};
+
 use bytes::{Buf, Bytes};
-use h3::{error::Code, proto::varint::VarInt, quic::StreamId, Error};
+use h3::{
+    connection::ConnectionInner,
+    error::Code,
+    proto::varint::VarInt,
+    quic::{self, StreamId},
+    Error,
+};
+use pin_project_lite::pin_project;
+
+use crate::quic_traits::RecvDatagramExt;
 
 /// HTTP datagram frames
 /// See: <https://www.rfc-editor.org/rfc/rfc9297#section-2.1>
@@ -65,5 +80,33 @@ where
     /// Returns the datagram payload
     pub fn into_payload(self) -> B {
         self.payload
+    }
+}
+
+pin_project! {
+    /// Future for [`Connection::read_datagram`]
+    pub struct ReadDatagram<'a, C, B>
+    where
+            C: quic::Connection<B>,
+            B: Buf,
+        {
+            pub(crate) conn: &'a mut ConnectionInner<C, B>,
+            pub(crate) _marker: PhantomData<B>,
+        }
+}
+
+impl<'a, C, B> Future for ReadDatagram<'a, C, B>
+where
+    C: quic::Connection<B> + RecvDatagramExt,
+    <C as RecvDatagramExt>::Error: h3::quic::Error + 'static,
+    B: Buf,
+{
+    type Output = Result<Option<Datagram<C::Buf>>, Error>;
+
+    fn poll(mut self: std::pin::Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match ready!(self.conn.conn.poll_accept_datagram(cx))? {
+            Some(v) => Poll::Ready(Ok(Some(Datagram::decode(v)?))),
+            None => Poll::Ready(Ok(None)),
+        }
     }
 }
